46b0f265d00e3465cc3f33442df60cc4afdd98b0
Esteban <gopovesteban2@gmail.com>
2019-03-12 21:25:30 -0600

    commit1

---
+++ dp2/dp2/src/main/java/com/FuSa/dp2/dp2/Dp2Application.java
@@ -0,0 +1,13 @@
+package com.FuSa.dp2.dp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dp2Application.class, args);
+	}
+
+}
